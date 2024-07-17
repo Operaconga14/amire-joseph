b469b7c1e4a6635feda851bcb00818400daf0a03
Operaconga14 <operacongahub@gmail.com>
2024-07-17 10:58:21 +0100

    Banner and About me Done and Seo configure

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from "../banner/banner.component";
+import { AboutComponent } from '../about/about.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { ContactComponent } from '../contact/contact.component';
+import { ExperienceComponent } from '../experience/experience.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [BannerComponent, AboutComponent, ProjectsComponent, ContactComponent, ExperienceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
