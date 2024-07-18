479e06c6e7ea70f4c6058dca3ad4eeab5475d607
Operaconga14 <operacongahub@gmail.com>
2024-07-18 11:46:14 +0100

    Done with experience and Projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DetailsService } from '../../services/details.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
 
+  personalProjects: any;
+  featuredProjects: any;
+
+  detailsService = inject(DetailsService)
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.showFeaturedProject()
+    this.showPersonalProject()
+  }
+
+  showFeaturedProject() {
+    this.featuredProjects = this.detailsService.featureProject
+  }
+
+  showPersonalProject() {
+    this.personalProjects = this.detailsService.personalProject
+  }
 }
--- src/app/services/details.service.ts
+++ src/app/services/details.service.ts
@@ -10,11 +10,43 @@ export class DetailsService {
   public stack: any;
   public yoe: any;
   public projects: any;
+  public personal: any
   public reachOut: any;
   public experience: any;
   public companies: any;
 
   constructor() { }
+
+  featureProject = [
+    {
+      name: 'Zswap',
+      description: ` Zswap is a decentralized application (dApp) built for seamless swapping of multi-chain and EVM-compatible tokens. Developed using Angular for a user-friendly and responsive frontend experience, and Solidity (Hardhat) for secure and robust smart contracts on the backend.`,
+      icon: 'bi bi-filetype-js'
+    },
+    {
+      name: 'Zsales',
+      description: ` Zsales is a user-friendly platform designed to streamline the creation of presales and launchpads for EVM-compatible tokens. Built with a powerful combination of Angular for the frontend and Solidity (Hardhat) for the backend smart contracts, Zsales empowers projects to raise capital securely and transparently.`,
+      icon: 'bi bi-browser-chrome'
+    },
+    {
+      name: 'Zarpay',
+      description: ` Zarpay is a revolutionary platform that simplifies the creation of online stores and empowers them to accept payments in EVM-compatible cryptocurrencies. Built with a user-friendly interface (Angular frontend) and secure smart contracts (Solidity with Hardhat), Zarpay provides a seamless experience for both store owners and customers.Zsales is a user-friendly platform designed to streamline the creation of presales and launchpads for EVM-compatible tokens. Built with a powerful combination of Angular for the frontend and Solidity (Hardhat) for the backend smart contracts, Zsales empowers projects to raise capital securely and transparently.`,
+      icon: 'bi bi-shop'
+    }
+  ]
+
+  personalProject = [
+    {
+      name: 'Color Gen-File',
+      description: `Color Gen-Fil is a Node.js command-line interface (CLI) tool that empowers you to generate custom color palettes and seamlessly integrate them into your web development projects.`,
+      img: "https://i.ibb.co/wh2MGqh/color-gen.png"
+    },
+    {
+      name: 'Qr Code Generator',
+      description: `Say hello to Qr Code Generator, your one-stop shop for creating and customizing QR codes! This user-friendly tool allows you to effortlessly transform any piece of information, whether it be a name, number, link, or even a symbol, into a scannable QR code. But that's not all - unleash your creativity and personalize your QR codes to perfectly match your needs and style.`,
+      img: "https://i.ibb.co/CMGyHDM/qr-Code-Gen.png"
+    }
+  ]
 
   company = [
     {
@@ -41,15 +73,14 @@ Provided website hosting solutions and designed user-friendly interfaces using W
     }
   ]
 
+
+
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.showCompanies()
-  }
-
-  showCompanies() {
     this.companies = this.company
-    console.log(this.companies)
+    this.projects = this.featureProject
+    this.personal = this.personalProject
   }
 
 }
