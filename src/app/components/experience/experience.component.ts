1b3d9e59a2bbcd6ad075c40f9b0074e00cf90477
Operaconga14 <operacongahub@gmail.com>
2024-07-18 09:10:32 +0100

    Changes to About me and Experience

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgbNavModule } from "@ng-bootstrap/ng-bootstrap";
+import { DetailsService } from '../../services/details.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [NgbNavModule],
+  imports: [NgbNavModule, CommonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent {
 
+  public detailsService = inject(DetailsService)
+
   zarclays: any;
   heirtrust: any;
   askit: any;
+  experiences: any;
 
-  active = 'zarclays'
+  active = 'heirtrust'
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.experiences = this.detailsService.companies
+  }
 
 }
--- src/app/services/details.service.ts
+++ src/app/services/details.service.ts
@@ -12,13 +12,44 @@ export class DetailsService {
   public projects: any;
   public reachOut: any;
   public experience: any;
+  public companies: any;
 
   constructor() { }
 
+  company = [
+    {
+      name: 'Zarclays',
+      role: 'Full-Stack Developer',
+      years: 'September 2021 - Present',
+      details: `Built and maintained web applications (ZSwap, Zsale, Zarpay) using Angular (frontend) and Solidity (Hardhat) (backend/smart contracts).
+Ensured secure and functional smart contract development for ZSwap, Zsale, and Zarpay.`
+    },
+    {
+      name: 'Heirtrust',
+      role: 'Full-Stack Developer',
+      years: 'September 2022 - Present',
+      details: `Developed and maintained secure web applications for managing wills and testaments, including file storage and cryptocurrency inheritance for next of kin.
+Utilized Angular for a user-friendly and responsive frontend experience.
+Implemented secure and robust smart contracts using Solidity (Hardhat) to facilitate secure file storage and crypto inheritance functionalities.`
+    },
+    {
+      name: 'Askit',
+      role: 'Coding Tutor and Web Desiner',
+      years: 'August 2021 - 2022',
+      details: `Developed engaging and educational applications for children using Scratch, a beginner-friendly programming language.
+Provided website hosting solutions and designed user-friendly interfaces using WordPress.`
+    }
+  ]
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+    this.showCompanies()
+  }
+
+  showCompanies() {
+    this.companies = this.company
+    console.log(this.companies)
   }
 
 }
