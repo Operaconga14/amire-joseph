cafc195c03990f78f18324ed5b1d8d4ecd7d0138
Operaconga14 <operacongahub@gmail.com>
2025-02-12 19:52:04 +0100

    new look and design remove services

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { DetailsService } from '../../services/details.service';
+import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
 import { BannerComponent } from "../banner/banner.component";
 import { ServicesComponent } from "../services/services.component";
@@ -15,18 +14,6 @@ import { ShortPortfolioComponent } from "../short-portfolio/short-portfolio.comp
 })
 export class HomeComponent {
 
-  skills: any;
 
-  detailsService = inject(DetailsService);
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.showSkills();
-  }
-
-  showSkills() {
-    this.skills = this.detailsService.skills
-  }
 
 }
