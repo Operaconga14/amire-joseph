66faaf89395411993e948ec7909b835c7f15eb3f
Operaconga14 <operacongahub@gmail.com>
2025-02-13 14:16:12 +0100

    Added some features

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+
+  currentRoute: string = '';
+
+  private activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.activatedRoute.url.subscribe(url => {
+      this.currentRoute = url.join('/');
+      console.log("Current Route: ", this.currentRoute);
+
+    });
+  }
+
+  shouldHideElement(): boolean {
+    return this.currentRoute === ''; // Replace 'desired-route' with the route you want to match
+  }
 
 }
